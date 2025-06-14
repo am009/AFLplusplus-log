4468704ee5b9c22b47eeacc86e9c5baf4e9a3d62
am009 <warrenwjk@qq.com>
2025-06-14 02:01:52 +0000

    Disable enhanced deterministic mode again.

--- src/afl-fuzz-one.c
+++ src/afl-fuzz-one.c
@@ -581,7 +581,7 @@ u8 fuzz_one_original(afl_state_t *afl) {
      we skip the whole deterministic stage as well */
 
   if (likely(afl->skip_deterministic) || likely(afl->queue_cur->passed_det) ||
-      likely(!afl->queue_cur->skipdet_e->quick_eff_bytes) ||
+      // likely(!afl->queue_cur->skipdet_e->quick_eff_bytes) ||
       likely(perf_score <
              (afl->queue_cur->depth * 30 <= afl->havoc_max_mult * 100
                   ? afl->queue_cur->depth * 30
