2aebcc47aa972ab5f5be0e45a56b49405ec465d4
am009 <warrenwjk@qq.com>
2025-08-28 16:13:02 +0000

    log cov-new-tuple/cov-new-count

--- src/afl-fuzz-bitmap.c
+++ src/afl-fuzz-bitmap.c
@@ -563,7 +563,7 @@ save_if_interesting(afl_state_t *afl, void *mem, u32 len, u8 fault) {
     }
 
     /* FUZZERLOG: log for new seed */
-    log_new_seed(queue_fn, "new-cov");
+    log_new_seed(queue_fn, new_bits == 0 ? "unknown" : (new_bits > 1 ? "cov-new-tuple" : "cov-new-count"));
     add_to_queue(afl, queue_fn, len, 0);
 
     if (unlikely(afl->fuzz_mode) &&
