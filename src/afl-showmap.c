4cb29ce6e8d91058cc3de3a7a212c74e89452609
am009 <warrenwjk@qq.com>
2026-01-14 04:10:02 +0000

    Fix showmap build.

--- src/afl-showmap.c
+++ src/afl-showmap.c
@@ -68,6 +68,13 @@
   #include <unistd.h>
 #endif
 
+void __fuzzerlog_empty() {}
+unsigned long long __fuzzerlog_empty1() { return 0; }
+
+start_exec_target_handle start_exec_target = __fuzzerlog_empty;
+end_exec_target_handle end_exec_target = __fuzzerlog_empty1;
+increase_chances_handle increase_chances = __fuzzerlog_empty;
+
 static afl_state_t *afl;
 
 static char *stdin_file;               /* stdin file                        */
